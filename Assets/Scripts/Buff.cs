7742a3e9c2e411f509398d6917e76ee329fa63a7
DESKTOP-PEJCES4\DeathToAllPigeons <pavkat97@gmail.com>
2018-01-27 17:48:31 +0200

    Started work on power ups.

---
+++ Assets/Scripts/Buff.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Buff : MonoBehaviour {
+
+    public float duration = 5.0f;
+    private float applyTime;
+    protected Character character;
+
+	public void Initialize(Character character) {
+
+        this.character = character;
+        applyTime = Time.time;
+        Apply();
+    }
+	
+    protected virtual void Apply() { }
+
+	// Update is called once per frame
+	void Update () {
+        if (Time.time >= applyTime + duration) {
+            UnApply();
+        }
+	}
+
+    protected virtual void UnApply() { }
+}
---
+++ Assets/Scripts/SwordRegenPowerUp.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwordRegenPowerUp : PowerUp {
+    public float buffValue = 0.1f;
+
+    protected override void Apply(Character character) {
+        character.swordSummonSpeed -= buffValue;
+        Debug.Log(character.swordSummonSpeed);
+    }
+}
