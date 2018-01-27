986c251471339a6421a6a4f0ba949f3cdb4f0d50
DESKTOP-PEJCES4\DeathToAllPigeons <pavkat97@gmail.com>
2018-01-28 01:29:07 +0200

    Started EnemySwarmer.

---
+++ Assets/Scripts/EnemySwarmer.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySwarmer : MonoBehaviour {
+
+    public GameObject enemyPrefab;
+    public int spawnRate = 1;
+    private Vector3 currentSpawnPosition;
+
+    private void FixedUpdate() {
+        
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
