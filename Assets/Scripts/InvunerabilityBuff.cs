77bd36eabac73eb091a2446bbe3a44dcd3ef3c00
DESKTOP-PEJCES4\DeathToAllPigeons <pavkat97@gmail.com>
2018-01-27 19:50:24 +0200

    Added invunerability buff.

--- Assets/Scripts/InvunerabilityBuff.cs
+++ Assets/Scripts/InvunerabilityBuff.cs
@@ -2,12 +2,14 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class InvunrabilityBuff : Buff {
+public class InvunerabilityBuff : Buff {
     protected override void Apply() {
         character.invulnrableCounter += 1;
+        Debug.Log("Apllying");
     }
 
     protected override void UnApply() {
         GameObject.FindGameObjectWithTag("Player").GetComponent<Character>().invulnrableCounter -= 1;
+        Debug.Log("UnApllying");
     }
 }
