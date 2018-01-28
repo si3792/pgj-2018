4240d0b8608c3afa121bd5b8c28a955b804dea25
Angel Paunov Georgiev <Georgiev1997@gmail.com>
2018-01-28 03:43:41 +0200

    Laid some groundwork on the pushback effect. Fixed the flicker bug with swords;

---
+++ Assets/Scripts/ObjectUtility/MeshLayerSelector.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MeshLayerSelector : MonoBehaviour {
+
+    public string layerName;
+    public int sortingOrder;
+
+    private void Awake() {
+        MeshRenderer renderer = GetComponent<MeshRenderer>();
+        renderer.sortingLayerName = layerName;
+        renderer.sortingOrder = sortingOrder;
+    }
+}
