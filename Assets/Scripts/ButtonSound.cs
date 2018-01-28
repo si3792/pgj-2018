03a9353009b2bb366b59a4f327eaf2e4d83d824e
DESKTOP-PEJCES4\DeathToAllPigeons <pavkat97@gmail.com>
2018-01-28 03:32:20 +0200

    Added sound manager and various sounds.

---
+++ Assets/Scripts/ButtonSound.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonSound : MonoBehaviour{
+
+    public AudioClip clickSoundEffect;
+    public AudioClip hoverSoundEffect;
+    private Button button;
+
+    private void Awake() {
+        button = GetComponent<Button>();
+    }
+
+    public void OnMouseDown() {
+        SoundManager.instance.PlayEffect(clickSoundEffect);
+    }
+
+    public void OnMouseOver() {
+        SoundManager.instance.PlayEffect(hoverSoundEffect);
+    }
+}
