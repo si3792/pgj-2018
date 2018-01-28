56f189266249e0b810ce74a75f33645857f59486
DESKTOP-PEJCES4\DeathToAllPigeons <pavkat97@gmail.com>
2018-01-28 06:17:49 +0200

    Added animated dick monster animations.

---
+++ Assets/Scripts/Boss.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boss : MonoBehaviour {
+    Animator animator;
+
+	// Use this for initialization
+	void Awake () {
+        animator = GetComponent<Animator>();
+	}
+	
+    public void Spawn() {
+        animator.ResetTrigger("Despawn");
+        animator.SetTrigger("Spawn");
+    }
+
+    public void Despawn() {
+        animator.ResetTrigger("Spawn");
+        animator.SetTrigger("Despawn");
+    }
+
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetButtonDown("Fire2")) {
+            Spawn();
+        }
+        if (Input.GetButtonDown("Fire3")) {
+            Despawn();
+        }
+    }
+}
